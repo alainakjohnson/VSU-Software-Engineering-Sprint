70b17bb51cb43c5c325ec0ff523e5212682bde32
Ryan Trull <rtrull97@gmail.com>
2018-09-07 14:40:49 -0400

    Added Constructor

--- Member.java
+++ Member.java
@@ -9,6 +9,14 @@ public class Member {
 	String emailAddress;
 	Scanner scan = new Scanner();
 
+   public Member(String firstName, String lastName, String screenName, String emailAddress, LocalDateTime dateCreated){
+   
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.screenName = screenName;
+      this.emailAddress = emailAddress;
+      this.dateCreated = dateCreated;
+   }
 
 	public String getEmailAddress(){
 
