342ee70d85e8661cd170e0c37cb5d14616cda6f9
jryoung718 <42520674+jryoung718@users.noreply.github.com>
2018-09-21 13:34:01 -0400

    modified group

--- sprint1/Group.java
+++ sprint1/Group.java
@@ -8,15 +8,16 @@ public class Group{
 	LocalDateTime dateCreated;
 	String title;
 	String description;
-	
+	List<Member> memberList = new ArrayList<Member>();
 	
 	
 	public Group(String title, String description, LocalDateTime dateCreated) {
-		final ArrayList<Member> memberList = new ArrayList<Member>();
+		//final ArrayList<Member> memberList = new ArrayList<Member>();
 		this.title = title;
 		this.description = description;
 		this.dateCreated = dateCreated;
-	}
+		
+	 }
 	
 	
 	public LocalDateTime getDateCreated() {
@@ -35,18 +36,35 @@ public class Group{
 		
 	}
 	
-//	public int getNumMembers() {
-//		return this.List<Members>.size;
-//	}
-//	
-//	public Member getMember(String emailAddress) {
-//		
-//	}
-//	
-//	public List<Member> getMembers(){
-//		
-//	}
-//	
+	public int getNumMembers() {
+		int numMem = 0;
+		for(int i = 0; i < memberList.size(); i++) {
+			if(memberList.get(i) != null) {
+				
+				numMem++;
+			}
+		}
+		return numMem;
+	}
+	
+	public Member getMember(String emailAddress) {
+		Member member = null;
+		for(int i = 0; i < memberList.size(); i++) {
+			
+			if(memberList.get(i).getEmailAddress().equals(emailAddress)) {
+				
+				member = memberList.get(i);
+			}
+			
+		}
+		return member;
+	}
+	
+	public List<Member> getMembers(){
+		
+		return memberList;
+	}
+	
 //	public List<Question> getQuestions(){
 //		
 //	}
