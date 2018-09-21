916c6bdadd04ccf45b34d82da609bbd88b600ef7
jryoung718 <42520674+jryoung718@users.noreply.github.com>
2018-09-21 13:36:00 -0400

    modified membership

--- sprint1/Membership.java
+++ sprint1/Membership.java
@@ -6,75 +6,36 @@ import java.util.List;
 
 class Membership{
    
-   private LocalDateTime dateJoined;
+   //dateJoined not yet implemented for joinGroup
+   //private LocalDateTime dateJoined;
    private int points;
    
   
    
    private static List<Group> groupList = new ArrayList<Group>();
-   private static List<Member> unassignedMembers;
-  // private static List<ArrayList<Member>> memberList = new ArrayList<ArrayList<Member>>(10);
+   //private static List<Member> unassignedMembers;
    
-  
-   public Membership(Group group){
-	   final ArrayList<Member> memberList = new ArrayList<Member>();
-	   LocalDateTime dateJoined;
-	   //storeGroup(group);  
-	   
-   }
-   
-   public static void joinAsMember(Group group) {
-	   
-	   
-   }
    public static void storeGroup(Group group) {
 	   
-	   //groupList.add(group);
+	   groupList.add(group);
 	   
    }
    
-//   //correlates with joinGroup method in member
-//   public static void storeMember(Group group, Member member) {
-//	   
-//	   for(int i = 0; i < groupList.size(); i++) {
-//		    
-//		  if(memberList.size() == 0) {
-//			  tempMemberList.add(member);
-//			  memberList.get(0).add((Member) tempMemberList);
-//			  
-//		  }
-//		  else if(group.getTitle().equals(groupList.get(i).getTitle())){
-//			  
-//			  memberList.get(i).add(member);
-//		  } 
-//			   
-//			   
-//		   
-//		   //memberList.add().add(member);
-//	   }
-//   }
-   
-//   public static List<Group> getNumG() {
-//	   
-//	   return groupList;
-//   }
-   
-//   public void addToGroup(Group group, Member member) {
-//	   
-//	   for(int i = 0; i < groupList.size(); i++) {
-//		   
-//		   if(groupList.get(i).equals(group)) {
-//			   
-//			   
-//		   }
-//	   }
-//	   
-//   }
-   
-//   public List<Member> giveMemberList(){
-//	   
-//	   
-//   }
+   public static Group retrieveGroup(String groupID) {
+	   
+	   Group group = new Group("Null", "Null", LocalDateTime.now());
+	   for(int i = 0; i< groupList.size(); i++) {
+		   
+		   if(groupList.get(i).getTitle().equals(groupID)) {
+			   
+			   group = groupList.get(i);
+			   
+			  
+		   }
+		   
+	   }
+	   return group;
+   }
    
    public static void displayGroupList(){
 	   System.out.println("============Group List================");
@@ -83,10 +44,24 @@ class Membership{
 	   }
 	   System.out.println("=======================================");
    }
-   public String toString(){
-
-	   return "1";
+   
+   public int totalNumGroups() {
+	   int numGroup = 0;
+	   for(int i = 0; i < groupList.size(); i++) {
+		   if(groupList.get(i) != null) {
+			   
+			   numGroup++;
+		   }
+		   
+	   }
+	   return numGroup;  
    }
+   
+//   public String toString(){
+//
+//	   return "Number of Groups: " + totalNumGroups()
+//			  + "\n" + displayGroupList();
+//   }
 
 
 
