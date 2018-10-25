cc5a4d30a75ee7cd12f38fadc0577bddd4fd7349
jryoung718 <42520674+jryoung718@users.noreply.github.com>
2018-10-25 11:12:19 -0400

    Update SiteManager

--- sprint2/SiteManager.java
+++ sprint2/SiteManager.java
@@ -1,5 +1,35 @@
-package sprint1;
+package sprint2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDateTime;
 
 public class SiteManager {
-
+	private List<Membership> members = new ArrayList<Membership>();
+	private List<Membership> groups = new ArrayList<Membership>();
+	
+	public SiteManager() {
+		
+	}
+	
+	public boolean addMember(String firstName, String lastName, String screenName, String emailAddress, LocalDateTime dateCreated) {
+		
+	}
+	
+	public Member getMember(String emailAddress) {
+		
+	}
+	
+	public List<Member> getMembers() {
+		
+	}
+	
+	public List<Member> getMembers(String text){
+		
+	}
+	
+	public boolean addGroup(String title, String description, LocalDateTime datecreated) {
+		
+	}
 }
+
