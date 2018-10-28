3d2ae9719ea80a04d0cab1d6dec1ce8071b92cfb
jryoung718 <42520674+jryoung718@users.noreply.github.com>
2018-10-28 16:43:24 -0400

    Updating PersistenceManager

--- sprint2/PersistenceManager.java
+++ sprint2/PersistenceManager.java
@@ -1,6 +1,12 @@
 package sprint2;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class PersistenceManager {
 	public PersistenceManager() {
@@ -8,10 +14,31 @@ public class PersistenceManager {
 	}
 	
 	public static boolean save(SiteManager sm, File file) {
-		FileOutputStream fos = new FileOutputStream("")
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("site.bin"));
+			oos.writeObject(sm);
+			oos.close();
+			return true;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;	
 	}
 	
 	public static SiteManager read(File file) {
-		
+		SiteManager sm = new SiteManager();
+		try {
+			FileInputStream fis = new FileInputStream(file);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			sm = (SiteManager) ois.readObject();
+			ois.close();
+			fis.close();
+			return sm;
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
