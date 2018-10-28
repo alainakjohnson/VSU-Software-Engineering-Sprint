223e31c009ff9e4b344bcfed6ca21c6ea7997992
jryoung718 <42520674+jryoung718@users.noreply.github.com>
2018-10-28 17:28:48 -0400

    updating

--- sprint2/PersistenceManager.java
+++ sprint2/PersistenceManager.java
@@ -15,7 +15,7 @@ public class PersistenceManager {
 	
 	public static boolean save(SiteManager sm, File file) {
 		try {
-			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("file"));
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
 			oos.writeObject(sm);
 			oos.close();
 			return true;
