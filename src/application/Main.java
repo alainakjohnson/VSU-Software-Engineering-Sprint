4baff33e6fe7a30af87eff2e3adfd77e733a790d
Alaina Johnson <alainakjohnson@gmail.com>
2018-12-05 19:16:14 -0500

    Corrected the Main class problem

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,5 +14,8 @@ public class Main extends Application {
 	        primaryStage.setScene(new Scene(root, 1000, 700));
 	        primaryStage.show();
 	    }
+	    public static void main (String[] args) {
+	    	launch(args);
+	    }
 	   
 }
